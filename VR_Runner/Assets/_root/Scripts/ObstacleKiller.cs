70b90244bacdf0dcf0b94d88a02d34fab5c7c24d
Budgo <Budgo@mail.ru>
2022-12-25 20:02:26 +0300

    Урок

---
+++ VR_Runner/Assets/_root/Scripts/ObstacleKiller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _root.Scripts
+{
+    public class ObstacleKiller : MonoBehaviour
+    {
+        [SerializeField] private Transform _plauer;
+        [SerializeField] private ObstacleSpawner _spawner;
+        [SerializeField] private float _killDisnranceZ;
+
+        private void Update()
+        {
+            List<Transform> obstacles = _spawner.spawnedObstacles;
+            for (int i = 0; i < obstacles.Count; i++)
+            {
+                if (_plauer.position.z > obstacles[i].position.z + _killDisnranceZ)
+                    Destroy(obstacles[i].gameObject);
+            }
+        }
+    }
+}
---
+++ VR_Runner/Assets/_root/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+namespace _root.Scripts
+{
+    public class ObstacleSpawner : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _obstacle;
+        [SerializeField] private float _spawnStep;
+        [SerializeField] private float _spawnDistance;
+
+        [SerializeField] private Vector2 _sergmentWidth;
+
+        private Transform _myTrans;
+        private Vector3 _lastPos;
+
+        private List<Transform> _spawnedObstacles = new List<Transform>();
+
+        public List<Transform> spawnedObstacles
+        {
+            get
+            {
+                _spawnedObstacles.RemoveAll(TransformIsNull);
+                return _spawnedObstacles;
+            }
+        }
+
+        bool TransformIsNull(Transform a)
+        {
+            return a == null;
+        }
+
+        private void Start()
+        {
+            _myTrans = transform;
+            _lastPos = _myTrans.position;
+        }
+
+        private void Update()
+        {
+            if (_myTrans.position.z > _lastPos.z + _spawnStep)
+            {
+                _lastPos.z += _spawnStep;
+                Transform newObstacle = _obstacle[Random.Range(0, _obstacle.Length)];
+                
+                _spawnedObstacles.Add(Instantiate(newObstacle, new Vector3(Random.Range(_sergmentWidth.x, _sergmentWidth.y),
+                   0, _lastPos.z + _spawnDistance ), Quaternion.identity));
+            }
+        }
+    }
+}

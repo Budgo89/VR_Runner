4a999180fbe9381350fb4ee344fc183f595fa393
Budgo <Budgo@mail.ru>
2023-01-04 19:00:36 +0300

    Задание 2

---
+++ VR_Runner/Assets/_root/Scripts/Lesson2/MyTeleport.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using HTC.UnityPlugin.Vive;
+using UnityEngine;
+
+namespace _root.Scripts.Lesson2
+{
+    public class MyTeleport : Teleportable
+    {
+        [SerializeField] private float Speed;
+        [SerializeField] private float CoolDoown;
+
+        public override IEnumerator StartTeleport(Vector3 position, float duration)
+        {
+            while (true)
+            {
+                yield return new WaitForFixedUpdate();
+
+                target.rotation = Vector3.MoveTowards(target.position, position, Speed * Time.deltaTime);
+
+                Vector3 v = position;
+                v.y = target.position.y;
+
+                if (Vector3.Distance(target.position, v) < 0.1f)
+                {
+                    yield return new WaitForSeconds(CoolDoown);
+                    teleportCoroutine = null;
+                    yield break;
+                }
+            }
+        }
+    }
+}
